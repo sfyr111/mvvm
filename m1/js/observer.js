fcba91713f4644b1f42136fbaa7b6d5e5a46c53c
sfyr111 <sfyr111@163.com>
2018-02-06 15:39:11 +0800

    定义订阅器Dep

--- m1/js/observer.js
+++ m1/js/observer.js
@@ -1,6 +1,7 @@
 /**
  *
  * @param data {object}
+ * @return void
  */
 function observe(data) {
   if (!data || typeof data !== 'object') return
@@ -14,10 +15,11 @@ function observe(data) {
  * @param data {object}
  * @param key {string}
  * @param val {any}
+ * @return void
  */
 function defineReactive(data, key, val) {
-  // 递归监听子属性
-  observe(val)
+  const dep = new Dep() // 消息订阅器
+  observe(val)  // 递归监听子属性
   Object.defineProperty(data, key, {
     enumerable: true, // 可枚举
     configurable: false, // key 属性不可删除
@@ -27,6 +29,25 @@ function defineReactive(data, key, val) {
     set(newVal) {
       console.log('监听ing: ', val, ' = ', newVal)
       val = newVal
+      dep.notify() // 通知所有订阅者
     }
   })
+}
+
+/**
+ * @name 消息订阅器
+ * @constructord subs {array} 收集订阅者
+ * @proto addSub {func} 添加订阅者
+ * @proto notify {func} 通知所有subs订阅者 -> 更新
+ */
+function Dep() {
+  this.subs = []
+}
+
+Dep.prototype.addSub = function(sub) {
+  this.subs.push(sub)
+}
+
+Dep.prototype.notify = function() {
+  this.subs.forEach(sub => sub.update())
 }
